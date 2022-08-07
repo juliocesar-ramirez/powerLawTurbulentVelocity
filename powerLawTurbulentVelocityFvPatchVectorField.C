8d8869b5012cf842512d24863cfa411c2d04d60a
juliocesar-ramirez <julio.ramirez@pucp.pe>
2022-08-06 22:31:01 -0500

    falta variable random

--- powerLawTurbulentVelocityFvPatchVectorField.C
+++ powerLawTurbulentVelocityFvPatchVectorField.C
@@ -23,11 +23,21 @@ License
 
 \*---------------------------------------------------------------------------*/
 
-#include "powerLawTurbulentVelocityFvPatchVectorField.H"
+#include "DimensionedScalarField.H"
+#include "GeometricScalarField.H"
 #include "addToRunTimeSelectionTable.H"
 #include "fvPatchFieldMapper.H"
-#include "volFields.H"
+#include "longDoubleScalar.H"
+#include "mathematicalConstants.H"
+#include "powerLawTurbulentVelocityFvPatchVectorField.H"
+#include "scalar.H"
+#include "scalarField.H"
 #include "surfaceFields.H"
+#include "vector.H"
+#include "vectorField.H"
+#include "volFields.H"
+#include <cmath>
+#include <math.h>
 
 // * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
 
@@ -42,19 +52,16 @@ Foam::scalar Foam::powerLawTurbulentVelocityFvPatchVectorField::t() const
 Foam::powerLawTurbulentVelocityFvPatchVectorField::
     powerLawTurbulentVelocityFvPatchVectorField(
         const fvPatch &p, const DimensionedField<vector, volMesh> &iF)
-    : fixedValueFvPatchVectorField(p, iF), scalarData_(0.0), a_(0.0),
-      alpha_(0.0), zref_(0.0), data_(Zero), n_(Zero), y_(Zero),
-      fieldData_(p.size(), Zero), timeVsData_(), wordData_("wordDefault"),
-      labelData_(-1), boolData_(false) {}
+    : fixedValueFvPatchVectorField(p, iF), a_(0.0), alpha_(0.0), zref_(0.0),
+      n_(Zero), y_(Zero), fieldData_(p.size(), Zero), timeVsData_(),
+      wordData_("wordDefault"), labelData_(-1), boolData_(false) {}
 
 Foam::powerLawTurbulentVelocityFvPatchVectorField::
     powerLawTurbulentVelocityFvPatchVectorField(
         const fvPatch &p, const DimensionedField<vector, volMesh> &iF,
         const dictionary &dict)
-    : fixedValueFvPatchVectorField(p, iF),
-      scalarData_(dict.lookup<scalar>("scalarData")),
-      a_(dict.lookup<scalar>("a")), alpha_(dict.lookup<scalar>("alpha")),
-      zref_(dict.lookup<scalar>("zref")), data_(dict.lookup<vector>("data")),
+    : fixedValueFvPatchVectorField(p, iF), a_(dict.lookup<scalar>("a")),
+      alpha_(dict.lookup<scalar>("alpha")), zref_(dict.lookup<scalar>("zref")),
       n_(dict.lookup<vector>("n")), y_(dict.lookup<vector>("y")),
       fieldData_("fieldData", dict, p.size()),
       timeVsData_(Function1<vector>::New("timeVsData", dict)),
@@ -77,9 +84,8 @@ Foam::powerLawTurbulentVelocityFvPatchVectorField::
         const powerLawTurbulentVelocityFvPatchVectorField &ptf,
         const fvPatch &p, const DimensionedField<vector, volMesh> &iF,
         const fvPatchFieldMapper &mapper)
-    : fixedValueFvPatchVectorField(ptf, p, iF, mapper),
-      scalarData_(ptf.scalarData_), a_(ptf.a_), alpha_(ptf.alpha_),
-      zref_(ptf.zref_), data_(ptf.data_), n_(ptf.n_), y_(ptf.y_),
+    : fixedValueFvPatchVectorField(ptf, p, iF, mapper), a_(ptf.a_),
+      alpha_(ptf.alpha_), zref_(ptf.zref_) , n_(ptf.n_), y_(ptf.y_),
       fieldData_(mapper(ptf.fieldData_)), timeVsData_(ptf.timeVsData_, false),
       wordData_(ptf.wordData_), labelData_(-1), boolData_(ptf.boolData_) {}
 
@@ -87,9 +93,9 @@ Foam::powerLawTurbulentVelocityFvPatchVectorField::
     powerLawTurbulentVelocityFvPatchVectorField(
         const powerLawTurbulentVelocityFvPatchVectorField &ptf,
         const DimensionedField<vector, volMesh> &iF)
-    : fixedValueFvPatchVectorField(ptf, iF), scalarData_(ptf.scalarData_),
-      a_(ptf.a_), alpha_(ptf.alpha_), zref_(ptf.zref_), data_(ptf.data_),
-      n_(ptf.n_), y_(ptf.y_), fieldData_(ptf.fieldData_),
+    : fixedValueFvPatchVectorField(ptf, iF), a_(ptf.a_),
+      alpha_(ptf.alpha_), zref_(ptf.zref_), n_(ptf.n_),
+      y_(ptf.y_), fieldData_(ptf.fieldData_),
       timeVsData_(ptf.timeVsData_, false), wordData_(ptf.wordData_),
       labelData_(-1), boolData_(ptf.boolData_) {}
 
@@ -127,11 +133,34 @@ void Foam::powerLawTurbulentVelocityFvPatchVectorField::updateCoeffs()
         return;
     }
 
-    fixedValueFvPatchVectorField::operator==
-    (
-        data_
-    );
+    vectorField cellxyz = patch().Cf();
+    scalarField celly = cellxyz & y_;
 
+    // power law
+    vectorField Upl = n_ * a_ * pow(celly, alpha_);
+
+    // velocidad turbulenta
+    scalarField Af=patch().magSf();
+    scalar A=gSum(Af);
+
+    scalar z0=zref_/exp(1/alpha_);
+    scalarField Iu=1/(log(celly/1));
+    scalarField k=1.5*pow(Upl.component(0),2)*pow(Iu,2);
+
+    scalarField circulacion = 4 *sqrt(Foam::constant::mathematical::pi*A*k)/(3*(2*log(3.0)-3*log(2.0)));
+    scalarField Utx = (1 / (Foam::constant::mathematical::twoPi)) * circulacion;
+    vector Ut = Zero;
+
+    boundBox bb(patch().patch().localPoints(), true);
+    vector bmax=bb.max();
+    scalar ly=bmax[1];
+    scalar lz=bmax[2];
+    //turbulent lenght
+    scalar L=(2*ly*lz)/(ly+ly);
+    scalar sigma=0.1*L/2;
+    scalar sigma2=2*sqr(sigma);
+
+    fixedValueFvPatchVectorField::operator==(Upl + Ut);
 
     fixedValueFvPatchVectorField::updateCoeffs();
 }
@@ -146,8 +175,6 @@ void Foam::powerLawTurbulentVelocityFvPatchVectorField::write
     writeEntry(os, "a", a_);
     writeEntry(os, "alpha", alpha_);
     writeEntry(os, "zref", zref_);
-    writeEntry(os, "scalarData", scalarData_);
-    writeEntry(os, "data", data_);
     writeEntry(os, "n", n_);
     writeEntry(os, "y", y_);
     writeEntry(os, "fieldData", fieldData_);
